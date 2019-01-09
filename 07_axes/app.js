6e5d1a81181cb8574a638a2b69016ea0d1f3df47
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-01-08 19:05:26 -0500

    Base code for axes

---
+++ 07_axes/app.js
@@ -0,0 +1,46 @@
+const dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+
+const svgWidth = 500,
+  svgHeight = 300,
+  barPadding = 5;
+const barWidth = svgWidth / dataset.length;
+
+// change svg size
+const svg = d3
+  .select("svg")
+  .attr("class", "bar-chart")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
+
+const yScale = d3
+  .scaleLinear()
+  .domain([0, d3.max(dataset)])
+  .range([0, svgHeight]);
+
+const barChart = svg
+  .selectAll("rect")
+  .data(dataset)
+  .enter()
+  .append("rect")
+  .attr("y", d => svgHeight - d)
+  .attr("height", d => d)
+  .attr("width", barWidth - barPadding)
+  .attr("transform", function(d, i) {
+    var translate = [barWidth * i, 0];
+    return "translate(" + translate + ")";
+  });
+
+// Adding text to all bar charts
+const text = svg
+  .selectAll("text")
+  .data(dataset)
+  .enter()
+  .append("text")
+  .text(d => d)
+  .attr("y", function(d, i) {
+    return svgHeight - d - 2;
+  })
+  .attr("x", function(d, i) {
+    return barWidth * i;
+  })
+  .attr("fill", "#A64C38");
